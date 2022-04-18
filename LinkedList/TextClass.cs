9686f563f1650da24b70c70a57b4d8aafbea9463
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-04-18 11:52:27 -0700

    Added Function Stubs

--- LinkedList/TextClass.cs
+++ LinkedList/TextClass.cs
@@ -6,5 +6,64 @@ namespace TextClassNamespace
 {
     class TextClass
     {
+        public TextClass()
+        {
+            throw new NotImplementedException();
+        }
+        public void addHead(char value)
+        {
+            throw new NotImplementedException();
+        }
+        public void addTail (char value)
+        {
+            throw new NotImplementedException();
+        }
+        public char getHead()
+        {
+            throw new NotImplementedException();
+        }
+        public char getTail()
+        {
+            throw new NotImplementedException();
+        }
+        public void removeHead()
+        {
+            throw new NotImplementedException();
+        }
+        public void removeTail()
+        {
+            throw new NotImplementedException();
+        }
+        public bool findValue(char value)
+        {
+            throw new NotImplementedException();
+        }
+        public bool findRemove(char value)
+        {
+            throw new NotImplementedException();
+        }
+        public string displayList()
+        {
+            throw new NotImplementedException();
+        }
+        public void append(TextClass otherList)
+        {
+            throw new NotImplementedException();
+        }
+        public void findNext(char value)
+        {
+            throw new NotImplementedException();
+        }
+        public void removeLast()
+        {
+            throw new NotImplementedException();
+        }
+        public void insertLast(char value)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
     }
 }
